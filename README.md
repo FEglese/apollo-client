5fcab53d32c33844fd20c9d1a12fa6f13fea070e
FredrikEglese <fredrik.eglese@gmail.com>
2021-10-09 15:59:09 +0100

    Add apollo client into app

--- README.md
+++ README.md
@@ -26,3 +26,13 @@ If you don't have serve locally, run `npm i -g serve` then deploy with `serve -s
 ### Runnin the test suite
 
 `npm run test`
+
+## To-do List
+
+> Any component tasks must also include tests
+
+- [ ] GraphQL / ApolloClient service for collecting data
+- [ ] Feed list component
+- [ ] Feed list item component
+- [ ] Search component
+- [ ] Ability to save previous searches
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
 import App from "./App";
+
+const apolloClient = new ApolloClient({
+	uri: "https://api.spacex.land/graphql/",
+	cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<ApolloProvider client={apolloClient}>
+			<App />
+		</ApolloProvider>
 	</React.StrictMode>,
 	document.getElementById("root")
 );
