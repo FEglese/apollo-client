854d148f36fea643202052d2e9f3d0fa01a2b1e9
FredrikEglese <fredrik.eglese@gmail.com>
2021-10-10 16:57:05 +0100

    Fix build

--- src/hooks/useOffsetCounter.ts
+++ src/hooks/useOffsetCounter.ts
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 export const useOffsetCounter = (initialVal: number, pageLength: number) => {
 	const [pageOffset, setPageOffset] = useState(initialVal);
